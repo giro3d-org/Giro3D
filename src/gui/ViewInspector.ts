d01287b190b27e9e939f64d2631d6e840bda946c
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-17 09:58:10 +0200

    feat(ViewInspector): add button create frustum snapshots

--- src/gui/ViewInspector.ts
+++ src/gui/ViewInspector.ts
@@ -1,12 +1,13 @@
 import type GUI from 'lil-gui';
-import type { OrthographicCamera, PerspectiveCamera, Vector3 } from 'three';
-import Panel from './Panel';
+import { CameraHelper, type OrthographicCamera, type PerspectiveCamera, type Vector3 } from 'three';
 import type Instance from '../core/Instance';
 import type View from '../renderer/View';
+import Panel from './Panel';
 
 class CameraInspector extends Panel {
     view: View;
     camera: PerspectiveCamera | OrthographicCamera;
+    snapshots: CameraHelper[] = [];
 
     /**
      * @param gui - The GUI.
@@ -32,6 +33,8 @@ class CameraInspector extends Panel {
             .onChange(notify);
         this.addController<number>(this.view, 'width').name('Width (pixels)');
         this.addController<number>(this.view, 'height').name('Height (pixels)');
+        this.addController<void>(this, 'createFrustumSnapshot').name('Create frustum snapshot');
+        this.addController<void>(this, 'deleteSnapshots').name('Delete frustum snapshots');
 
         const position = this.gui.addFolder('Position');
         position.close();
@@ -47,6 +50,23 @@ class CameraInspector extends Panel {
             this._controllers.push(target.add(targetObj, 'y'));
             this._controllers.push(target.add(targetObj, 'z'));
         }
+    }
+
+    private deleteSnapshots() {
+        this.snapshots.forEach(helper => {
+            helper.dispose();
+            this.instance.remove(helper);
+        });
+        this.snapshots.length = 0;
+    }
+
+    private createFrustumSnapshot() {
+        const helper = new CameraHelper(this.instance.view.camera);
+        this.instance.add(helper);
+        helper.update();
+        this.instance.notifyChange();
+        helper.updateMatrixWorld(true);
+        this.snapshots.push(helper);
     }
 }
 
