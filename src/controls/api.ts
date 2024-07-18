3e0a6736567d1b4c17ea18d0e2d6fcc543636c1b
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-18 16:43:28 +0200

    feat(controls): add GlobeControls

---
+++ src/controls/api.ts
@@ -0,0 +1,5 @@
+import type FirstPersonControls from './FirstPersonControls';
+import type { FirstPersonControlsOptions } from './FirstPersonControls';
+import type GlobeControls from './GlobeControls';
+
+export { FirstPersonControls, FirstPersonControlsOptions, GlobeControls };
