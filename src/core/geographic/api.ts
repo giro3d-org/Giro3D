6d2b2c84c1e4975cc5cfc4c4759b59e8fb8659ca
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-25 18:33:51 +0200

    feat(Sun): add functions to compute the position of sun

--- src/core/geographic/api.ts
+++ src/core/geographic/api.ts
@@ -2,5 +2,6 @@ import type Coordinates from './Coordinates';
 import type { CoordinateParameters } from './Coordinates';
 import type Extent from './Extent';
 import type { ExtentParameters } from './Extent';
+import type Sun from './Sun';
 
-export { CoordinateParameters, Coordinates, Extent, ExtentParameters };
+export { CoordinateParameters, Coordinates, Extent, ExtentParameters, Sun };
