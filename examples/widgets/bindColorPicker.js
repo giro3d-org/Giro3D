909600160cab61b5b5a7190fd139fd0f8a251863
Sébastien Guimmara <sguimmara@pobox.com>
2024-08-08 13:22:20 +0200

    fix(bindColorPicker): accept values of type ColorRepresentation

--- examples/widgets/bindColorPicker.js
+++ examples/widgets/bindColorPicker.js
@@ -1,14 +1,23 @@
+import { Color, ColorRepresentation } from 'three';
+
+/**
+ * @param {string} id - The DOM element id.
+ * @param {(color: Color) => void} onChange - The change callback.
+ * @returns {(color: ColorRepresentation) => void} The external update function.
+ */
 export function bindColorPicker(id, onChange) {
     const colorPicker = document.getElementById(id);
 
     colorPicker.oninput = function oninput() {
         // Let's change the classification color with the color picker value
         const hexColor = colorPicker.value;
-        onChange(hexColor);
+        onChange(new Color(hexColor));
     };
 
-    return v => {
-        colorPicker.value = v;
+    const externalFunction = v => {
+        colorPicker.value = `#${new Color(v).getHexString()}`;
         onChange(colorPicker.value);
     };
+
+    return externalFunction;
 }
