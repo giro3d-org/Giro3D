ff5c177f76f3bb76e9776b249903905e53960054
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-09-09 14:09:19 +0200

    docs(Instance): fix documentation of pickObjectsAt()

--- src/core/Instance.ts
+++ src/core/Instance.ts
@@ -819,10 +819,11 @@ class Instance extends EventDispatcher<InstanceEvents> implements Progress {
      * layer/source, there may be additionnal properties (coming from THREE.Raycaster
      * for instance).
      * If `options.pickFeatures` if `true`, `features` property may be set.
+     *
      * ```js
-     * instance.pickObjectsAt({ x, y })
-     * instance.pickObjectsAt({ x, y }, { radius: 1, where: ['wfsBuilding'] })
-     * instance.pickObjectsAt({ x, y }, { radius: 3, where: ['wfsBuilding', myLayer] })
+     * instance.pickObjectsAt(mouseEvent)
+     * instance.pickObjectsAt(mouseEvent, { radius: 1, where: [entity0, entity1] })
+     * instance.pickObjectsAt(mouseEvent, { radius: 3, where: [entity0] })
      * ```
      */
     pickObjectsAt(
