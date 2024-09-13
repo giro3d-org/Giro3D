8829167b6f42ddcfbbf8b28fff1345606ec7b1d6
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-09-13 08:39:23 +0200

    docs(layer_blending_modes): fix incorrect layout of long description

--- src/core/layer/BlendingMode.ts
+++ src/core/layer/BlendingMode.ts
@@ -1,18 +1,25 @@
+/**
+ * Describes how a layer is blended into the background (either another layer or the background
+ * color).
+ */
 enum BlendingMode {
     /**
-     * Discard layer transparency.
+     * No blending is performed, i.e if the layer has transparent or semi-transparent pixels,
+     * they become opaque.
      */
     None = 0,
     /**
-     * Normal alpha blending.
+     * Normal [alpha blending](https://en.wikipedia.org/wiki/Alpha_compositing).
      */
     Normal = 1,
     /**
-     * Additive blending.
+     * Additive blending: pixel colors are added to the background pixel. The alpha channel is not
+     * used
      */
     Add = 2,
     /**
-     * Multiplicative blending.
+     * Multiplicative blending: pixel colors are multiplied by the background pixel. The alpha
+     * channel is not used.
      */
     Multiply = 3,
 }
