b195f9547c75fa0fd58e60c179f15b6494fc10af
Sébastien Guimmara <sguimmara@pobox.com>
2024-09-14 10:54:39 +0200

    refactor(ColorUtils): getContrastColor() now accepts a ColorRepresentation

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -1,13 +1,15 @@
-import type { Color } from 'three';
+import type { ColorRepresentation } from 'three';
+import { Color } from 'three';
 
 // See https://www.codeproject.com/Articles/16565/Determining-Ideal-Text-Color-Based-on-Specified-Ba
 /**
- * Returns a color that contrasts with the input color.
+ * Returns a hex color (including the leading #) that contrasts with the input color.
  */
-export function getContrastColor(color: Color): string {
+export function getContrastColor(color: ColorRepresentation): string {
+    const c = color instanceof Color ? color : new Color(color);
     // Find a text color with enough contrast from the background color
     const nThreshold = 105 / 255;
-    const bgDelta = color.r * 0.299 + color.g * 0.587 + color.b * 0.114;
+    const bgDelta = c.r * 0.299 + c.g * 0.587 + c.b * 0.114;
 
     return 1 - bgDelta < nThreshold ? '#000000' : '#ffffff';
 }
