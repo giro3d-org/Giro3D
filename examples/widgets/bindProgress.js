4e9240c58684c6c0630ddbe9acf9fcda9131a483
Sébastien Guimmara <sguimmara@pobox.com>
2024-09-19 11:31:10 +0200

    docs(widgets): add bindProgress

---
+++ examples/widgets/bindProgress.js
@@ -0,0 +1,23 @@
+/**
+ * Binds a progress bar.
+ * @param {string} id The id of the <div> element.
+ * @returns {[(value: number, text?: string) => void, HTMLElement]} An array containing the function
+ * to update the progress value (the width of the progress bar), and the progress bar element.
+ */
+export function bindProgress(id) {
+    const element = document.getElementById(id);
+    if (!(element instanceof HTMLDivElement)) {
+        throw new Error(
+            'invalid binding element: expected HTMLDivElement, got: ' + element.constructor.name,
+        );
+    }
+
+    const setProgress = (normalized, text) => {
+        element.style.width = `${Math.round(normalized * 100)}%`;
+        if (text) {
+            element.innerText = text;
+        }
+    };
+
+    return [setProgress, element.parentElement];
+}
