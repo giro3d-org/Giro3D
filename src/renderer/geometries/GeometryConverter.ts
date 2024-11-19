c8fd810233a8364375b0b7a3ff27120c00a341d8
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-11-19 15:39:43 +0100

    fix(GeometryConverter): support triangulation of vertical polygons (#544)
    Vertices are transformed into the polygon's plane so that the earcut
    algorithm "sees" them as lying on the XY (horizontal) plane.
    
    fix #544

--- src/renderer/geometries/GeometryConverter.ts
+++ src/renderer/geometries/GeometryConverter.ts
@@ -15,8 +15,10 @@ import {
     BufferGeometry,
     DoubleSide,
     EventDispatcher,
+    Matrix4,
     MeshBasicMaterial,
     MeshLambertMaterial,
+    Plane,
     SpriteMaterial,
     SRGBColorSpace,
     Vector3,
@@ -132,9 +134,9 @@ function createFloorVertices(params: {
     ignoreZ: boolean;
 }) {
     // iterate on polygon and holes
-    const holesIndices = [];
+    const holesIndices: number[] = [];
     let currentIndex = 0;
-    const positions = [];
+    const positions: number[] = [];
 
     const { coordinates, offset, ignoreZ, elevation, stride } = params;
 
@@ -269,6 +271,60 @@ function createWallForRings(
     }
 }
 
+const tris = {
+    a: new Vector3(),
+    b: new Vector3(),
+    c: new Vector3(),
+    matrix: new Matrix4(),
+    plane: new Plane(),
+    tempV: new Vector3(),
+    UP: new Vector3(0, 0, 1),
+};
+
+function triangulate(flatCoordinates: ArrayLike<number>, holeIndices: ArrayLike<number>) {
+    const coord = flatCoordinates;
+
+    tris.a.set(coord[0], coord[1], coord[2]);
+    tris.b.set(coord[3], coord[4], coord[5]);
+    tris.c.set(coord[6], coord[7], coord[8]);
+
+    // Since the earcut algorithm only works on the XY plane, any vertical polygon (e.g walls)
+    // will not be correctly processed. We thus have to transform them as if they were sitting
+    // (roughly) on the XY plane.
+    // To do this, we compute the plane that the vertices are on, then create a matrix that has
+    // the same orientation as this plane, then transform all points using this matrix.
+    const plane = tris.plane.setFromCoplanarPoints(tris.a, tris.b, tris.c);
+
+    // Is the plane completely orthogonal to the horizontal plane ?
+    const dot = Math.abs(plane.normal.dot(tris.UP));
+
+    // A dot product equals to zero means that the two planes are orthogonal (i.e all points of the
+    // polygon plane, when projected onto the horizontal plane, will form a straight line, without any area)
+    // Even if the area is very small, it is generally sufficient for the earcut algorithm to work.
+    if (Math.abs(dot) > 0.05) {
+        // Don't bother to transform coordinates since the polygon is on the XY plane.
+        return Earcut(flatCoordinates, holeIndices, 3);
+    }
+
+    const matrix = tris.matrix.lookAt(tris.a, plane.normal.add(tris.a), new Vector3(0, 0, 1));
+
+    const adjustedCoordinates = new Float64Array(flatCoordinates.length);
+
+    for (let i = 0; i < flatCoordinates.length; i += 3) {
+        const x = flatCoordinates[i + X];
+        const y = flatCoordinates[i + Y];
+        const z = flatCoordinates[i + Z];
+
+        const v = tris.tempV.set(x, y, z).applyMatrix4(matrix);
+
+        adjustedCoordinates[i + X] = v.y;
+        adjustedCoordinates[i + Y] = v.x;
+        adjustedCoordinates[i + Z] = v.z;
+    }
+
+    return Earcut(adjustedCoordinates, holeIndices, 3);
+}
+
 function createSurfaces(polygon: Polygon, options: PolygonOptions) {
     const stride = polygon.getStride();
 
@@ -286,7 +342,7 @@ function createSurfaces(polygon: Polygon, options: PolygonOptions) {
 
     const pointCount = flatCoordinates.length / 3;
 
-    const triangles = Earcut(flatCoordinates, holes, 3);
+    const triangles = triangulate(flatCoordinates, holes);
 
     if (options.extrusionOffset != null) {
         createRoof(flatCoordinates, pointCount, triangles, options.extrusionOffset);
