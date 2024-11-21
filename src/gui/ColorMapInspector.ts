fd49ceab8d78bb2c94518bcfc871bd95b505453a
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-11-21 17:31:53 +0100

    feat(ColorMapInspector): add support for dynamically updated color maps

--- src/gui/ColorMapInspector.ts
+++ src/gui/ColorMapInspector.ts
@@ -1,51 +1,69 @@
 import type GUI from 'lil-gui';
 import type Instance from '../core/Instance';
-import type ColorMap from '../core/layer/ColorMap';
+import ColorMap from '../core/layer/ColorMap';
 import Panel from './Panel';
 
 type Mode = 'Elevation' | 'Slope' | 'Aspect';
 
 const modes: Mode[] = ['Elevation', 'Slope', 'Aspect'];
 
+const DEFAULT_COLORMAP = new ColorMap([], 0, 1);
+
 /**
  * Inspector for a {@link ColorMap}.
  */
 class ColorMapInspector extends Panel {
-    mode: Mode = 'Elevation';
+    private readonly _notify: () => void;
+    private readonly _colorMapFn: () => ColorMap | null;
+
+    get colorMap() {
+        return this._colorMapFn() ?? DEFAULT_COLORMAP;
+    }
+
+    get mode() {
+        return modes[this.colorMap.mode - 1];
+    }
+
+    set mode(v: Mode) {
+        this.colorMap.mode = modes.indexOf(v) + 1;
+    }
 
     /**
      * @param gui - The GUI.
      * @param instance - The Giro3D instance.
      * @param layer - The color map owner.
-     * @param colorMap - The color map to inspect.
+     * @param colorMapFn - The color map to inspect.
      */
-    constructor(gui: GUI, instance: Instance, colorMap: ColorMap | null, notify: () => void) {
+    constructor(
+        gui: GUI,
+        instance: Instance,
+        colorMapFn: () => ColorMap | null,
+        notify: () => void,
+    ) {
         super(gui, instance, 'Color map');
+        this._notify = notify;
+        this._colorMapFn = colorMapFn;
 
-        if (colorMap != null) {
-            this.mode = modes[colorMap.mode - 1];
+        this.addController(this.colorMap, 'active').name('Enabled').onChange(notify);
 
-            this.addController(colorMap, 'active').name('Enabled').onChange(notify);
+        this.addController(this, 'mode', modes).name('Mode').onChange(notify);
 
-            this.addController(this, 'mode', modes)
-                .name('Mode')
-                .onChange(v => {
-                    colorMap.mode = modes.indexOf(v) + 1;
-                    notify();
-                });
+        this.addController(this.colorMap, 'min')
+            .name('Lower bound')
+            .min(-8000)
+            .max(8000)
+            .onChange(notify);
+        this.addController(this.colorMap, 'max')
+            .name('Upper bound')
+            .min(-8000)
+            .max(8000)
+            .onChange(notify);
+    }
 
-            this.addController(colorMap, 'min')
-                .name('Lower bound')
-                .min(-8000)
-                .max(8000)
-                .onChange(notify);
-
-            this.addController(colorMap, 'max')
-                .name('Upper bound')
-                .min(-8000)
-                .max(8000)
-                .onChange(notify);
-        }
+    updateControllers(): void {
+        const disabled = this._colorMapFn() == null;
+        this._controllers.forEach(c => c.disable(disabled));
+        super.updateControllers();
     }
 }
 
