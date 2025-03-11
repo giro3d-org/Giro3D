0ab51366393de78e61914aca2b2c7e47a66fea58
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-03-11 09:43:06 +0100

    refactor(MapInspector): add toggle for horizon culling in Globes

--- src/gui/MapInspector.ts
+++ src/gui/MapInspector.ts
@@ -2,6 +2,7 @@ import type GUI from 'lil-gui';
 import type { AxesHelper, GridHelper, Side } from 'three';
 import { Color, MathUtils, Mesh, MeshBasicMaterial, Sphere, SphereGeometry } from 'three';
 import type Instance from '../core/Instance';
+import Globe from '../entities/Globe';
 import type Map from '../entities/Map';
 import TileMesh from '../entities/tiles/TileMesh';
 import type { BoundingBoxHelper } from '../helpers/Helpers';
@@ -106,6 +107,9 @@ class MapInspector extends EntityInspector<Map> {
             this.addController(this.entity.elevationRange, 'max')
                 .name('Elevation range maximum')
                 .onChange(() => this.notify(map));
+        }
+        if (this.entity instanceof Globe) {
+            this.addController(this.entity, 'horizonCulling');
         }
         this.addController(this.entity, 'castShadow');
         this.addController(this.entity, 'receiveShadow');
