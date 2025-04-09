a3ea84286365b1931c9ede6064a5b1ef43c03fe2
Sébastien Guimmara <sguimmara@pobox.com>
2025-04-09 13:47:14 +0200

    build: don't export api.ts files in the generated package

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = {
         ['babel-plugin-inline-import', { extensions: ['.json', '.glsl'] }],
         ['minify-dead-code-elimination'],
     ],
+    // API barrel files are only there to generate the documentation
+    // We really don't want barrel files in the exported package
+    ignore: ['**/api.ts'],
     env: {
         test: {
             presets: ['jest', ['@babel/preset-env']],
