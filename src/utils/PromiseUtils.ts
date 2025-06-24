532480339d3ac46911a2428210aff2e089c1a61e
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2025-06-24 18:05:42 +0200

    feat(PromiseUtils): add batch() to split computations in several slices

--- src/utils/PromiseUtils.ts
+++ src/utils/PromiseUtils.ts
@@ -14,6 +14,10 @@ function delay(duration: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, duration));
 }
 
+function nextFrame(): Promise<void> {
+    return new Promise(resolve => requestAnimationFrame(_ => resolve()));
+}
+
 export enum PromiseStatus {
     Fullfilled = 'fulfilled',
     Rejected = 'rejected',
@@ -35,8 +39,61 @@ function abortError(): Error {
     return new AbortError();
 }
 
+const SLICE_DURATION_MILLISECONDS = 4;
+
+function batch<I, O>(
+    items: I[],
+    transformer: (obj: I, index: number) => O | null,
+    options?: {
+        outputItems?: O[];
+        start?: number;
+        signal?: AbortSignal;
+    },
+): Promise<O[]> {
+    const result: O[] = options?.outputItems ?? [];
+
+    const processSlice = (start: number) => {
+        const begin = performance.now();
+
+        for (let i = start; i < items.length; i++) {
+            const input = items[i];
+            const output = transformer(input, i);
+
+            if (output != null) {
+                result.push(output);
+            }
+
+            const elapsed = performance.now() - begin;
+
+            if (elapsed > SLICE_DURATION_MILLISECONDS) {
+                const nextStart = i + 1;
+                return Promise.resolve(nextStart);
+            }
+        }
+
+        options?.signal?.throwIfAborted();
+
+        return Promise.resolve(undefined);
+    };
+
+    return processSlice(options?.start ?? 0).then(async nextStart => {
+        if (nextStart != null) {
+            options?.signal?.throwIfAborted();
+
+            await batch(items, transformer, {
+                outputItems: result,
+                signal: options?.signal,
+                start: nextStart,
+            });
+        }
+        return result;
+    });
+}
+
 export default {
     delay,
     PromiseStatus,
     abortError,
+    nextFrame,
+    batch,
 };
