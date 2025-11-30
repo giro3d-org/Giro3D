4b099a0ec05a6a6a7fc68e8580a193ca9ec80501
Jérémie Piellard <jeremie.piellard@gmail.com>
2025-11-30 19:06:20 +0100

    refactor(Classification): extract file

--- src/renderer/PointCloudMaterial.ts
+++ src/renderer/PointCloudMaterial.ts
@@ -8,7 +8,6 @@ import type {
     BufferAttribute,
     BufferGeometry,
     Camera,
-    ColorRepresentation,
     IUniform,
     Scene,
     Texture,
@@ -17,7 +16,6 @@ import type {
 
 import {
     Color,
-    DataTexture,
     GLSL3,
     Matrix4,
     NoBlending,
@@ -37,8 +35,15 @@ import type { IntersectingVolume, IntersectingVolumesUniform } from './Intersect
 import ColorMap from '../core/ColorMap';
 import OffsetScale from '../core/OffsetScale';
 import MaterialUtils, { type VertexAttributeType } from './MaterialUtils';
+import {
+    ASPRS_CLASSIFICATIONS,
+    Classification,
+    ClassificationsTexture,
+} from './pointcloudmaterial/Classification';
 import PointsFS from './shader/PointsFS.glsl';
 import PointsVS from './shader/PointsVS.glsl';
+
+export { ASPRS_CLASSIFICATIONS, Classification, ClassificationsTexture };
 
 const tmpDims = new Vector2();
 
@@ -63,67 +68,6 @@ export enum MODE {
 export type Mode = (typeof MODE)[keyof typeof MODE];
 
 const NUM_TRANSFO = 16;
-
-/**
- * Paremeters for a point cloud classification.
- */
-export class Classification {
-    /**
-     * The color of this classification.
-     */
-    public color: Color;
-    /**
-     * Toggles the visibility of points with this classification.
-     */
-    public visible: boolean;
-
-    public constructor(color: ColorRepresentation, visible = true) {
-        this.color = new Color(color);
-        this.visible = visible;
-    }
-
-    /**
-     * Clones this classification.
-     * @returns The cloned object.
-     */
-    public clone(): Classification {
-        return new Classification(this.color.clone(), this.visible);
-    }
-}
-
-/**
- * A set of 256 pre-defined classifications following the ASPRS scheme, with pre-defined colors for
- * classifications 0 to 18. The remaining classifications have the default color (#FF8100)
- *
- * See https://www.asprs.org/wp-content/uploads/2010/12/LAS_Specification.pdf
- */
-export const ASPRS_CLASSIFICATIONS: Classification[] = new Array(256);
-
-const DEFAULT_CLASSIFICATION = new Classification(0xff8100);
-
-for (let i = 0; i < ASPRS_CLASSIFICATIONS.length; i++) {
-    ASPRS_CLASSIFICATIONS[i] = DEFAULT_CLASSIFICATION.clone();
-}
-
-ASPRS_CLASSIFICATIONS[0] = new Classification('#858585'); // Created, never classified
-ASPRS_CLASSIFICATIONS[1] = new Classification('#bfbfbf'); // Unclassified
-ASPRS_CLASSIFICATIONS[2] = new Classification('#834000'); // Ground
-ASPRS_CLASSIFICATIONS[3] = new Classification('#008100'); // Low vegetation
-ASPRS_CLASSIFICATIONS[4] = new Classification('#00bf00'); // Medium vegetation
-ASPRS_CLASSIFICATIONS[5] = new Classification('#00ff00'); // High vegetation
-ASPRS_CLASSIFICATIONS[6] = new Classification('#0081c1'); // Building
-ASPRS_CLASSIFICATIONS[7] = new Classification('#ff0000'); // Low point (noise)
-ASPRS_CLASSIFICATIONS[8] = DEFAULT_CLASSIFICATION.clone(); // Reserved
-ASPRS_CLASSIFICATIONS[9] = new Classification('#0000ff'); // Water
-ASPRS_CLASSIFICATIONS[10] = new Classification('#606d73'); // Rail
-ASPRS_CLASSIFICATIONS[11] = new Classification('#858585'); // Road surface
-ASPRS_CLASSIFICATIONS[12] = DEFAULT_CLASSIFICATION.clone(); // Reserved
-ASPRS_CLASSIFICATIONS[13] = new Classification('#ede440'); // Wire - Guard (Shield)
-ASPRS_CLASSIFICATIONS[14] = new Classification('#ed6840'); // Wire - Conductor (Phase)
-ASPRS_CLASSIFICATIONS[15] = new Classification('#29fff8'); // Transmission Tower
-ASPRS_CLASSIFICATIONS[16] = new Classification('#5e441d'); // Wire Structure connector (e.g Insulator)
-ASPRS_CLASSIFICATIONS[17] = new Classification('#7992c7'); // Bridge Deck
-ASPRS_CLASSIFICATIONS[18] = new Classification('#cd27d6'); // High Noise
 
 export interface PointCloudMaterialOptions {
     /**
@@ -223,67 +167,6 @@ export interface Defines extends Record<string, unknown> {
 function createDefaultColorMap(): ColorMap {
     const colors = [new Color('black'), new Color('white')];
     return new ColorMap({ colors, min: 0, max: 1000 });
-}
-
-class ClassificationsTexture {
-    public static readonly maxCount = 256;
-
-    public classifications: Classification[] = [];
-    public readonly uniform = new Uniform<Texture | null>(null);
-
-    private readonly _array = new Uint8Array(4 * ClassificationsTexture.maxCount);
-
-    public updateUniform(): void {
-        this.sanitizeClassifications();
-
-        if (!this.uniform.value) {
-            this.uniform.value = new DataTexture(
-                this._array as BufferSource,
-                ClassificationsTexture.maxCount,
-                1,
-            );
-        }
-
-        const temp = new Uint8Array(4);
-        for (let i = 0; i < ClassificationsTexture.maxCount; i++) {
-            const classification = this.classifications[i] ?? DEFAULT_CLASSIFICATION;
-            temp[0] = 255 * classification.color.r;
-            temp[1] = 255 * classification.color.g;
-            temp[2] = 255 * classification.color.b;
-            temp[3] = classification.visible ? 255 : 0;
-
-            let classifChanged = false;
-            const classifOffset = 4 * i;
-            for (let j = 0; j < 4; j++) {
-                if (this._array[classifOffset + j] !== temp[j]) {
-                    classifChanged = true;
-                    break;
-                }
-            }
-            if (classifChanged) {
-                this._array.set(temp, classifOffset);
-                this.uniform.value.needsUpdate = true;
-            }
-        }
-    }
-
-    public dispose(): void {
-        this.uniform.value?.dispose();
-        this.uniform.value = null;
-    }
-
-    private sanitizeClassifications(): void {
-        if (this.classifications.length > ClassificationsTexture.maxCount) {
-            this.classifications = this.classifications.slice(0, ClassificationsTexture.maxCount);
-            console.warn(
-                `The provided classification array has been truncated to ${ClassificationsTexture.maxCount} elements`,
-            );
-        } else {
-            while (this.classifications.length < ClassificationsTexture.maxCount) {
-                this.classifications.push(DEFAULT_CLASSIFICATION.clone());
-            }
-        }
-    }
 }
 
 /**
@@ -485,7 +368,6 @@ class PointCloudMaterial extends ShaderMaterial {
         this.needsUpdate = true;
 
         this._classificationsTexture = new ClassificationsTexture();
-        this._classificationsTexture.classifications = ASPRS_CLASSIFICATIONS.map(c => c.clone());
 
         this.uniforms = {
             fogDensity: new Uniform(0.00025),
