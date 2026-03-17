144aad5b15c3ab97f57ea63e689733aa0b5df70d
Sébastien Guimmara <sguimmara@pobox.com>
2026-03-17 13:44:17 +0100

    docs(API): remove api.ts barrel files

--- src/interactions/api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2015-2018, IGN France.
- * Copyright (c) 2018-2026, Giro3D team.
- * SPDX-License-Identifier: MIT
- */
-
-export * as DrawTool from './DrawTool';
-export * as SunExposure from './SunExposure';
