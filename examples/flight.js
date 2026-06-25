f57871530e27aa9e6180b162f6872a76f6f17e47
Thomas Muguet <thomas.muguet@oslandia.com>
2026-06-25 16:16:36 +0200

    perf(examples): use CubicInterpolant for path interpolation

--- examples/flight.js
+++ examples/flight.js
@@ -7,7 +7,7 @@
 import GeoJSON from 'ol/format/GeoJSON.js';
 import OSM from 'ol/source/OSM.js';
 import VectorSource from 'ol/source/Vector.js';
-import { OrthographicCamera } from 'three';
+import { CubicInterpolant, OrthographicCamera } from 'three';
 import { CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
 import Coordinates from '@giro3d/giro3d/core/geographic/Coordinates.js';
@@ -172,113 +172,40 @@ fetch('data/flight_path.geojson')
             return [reprojected.x, reprojected.y, reprojected.z];
         });
 
-        const samplePath = t => {
-            if (typeof t !== 'number' || isNaN(t)) {
-                t = 0;
-            }
+        const parameterPositions = new Float32Array(POINTS.length);
+        const sampleValues = new Float32Array(POINTS.length * 3);
+        for (let i = 0; i < POINTS.length; i++) {
+            // We'll interpolate with constant time between each values from the path
+            parameterPositions[i] = i / (POINTS.length - 1);
+            sampleValues[i * 3 + 0] = POINTS[i][0];
+            sampleValues[i * 3 + 1] = POINTS[i][1];
+            sampleValues[i * 3 + 2] = POINTS[i][2];
+        }
+        const interpolant = new CubicInterpolant(parameterPositions, sampleValues, 3);
 
-            const p0 = Math.floor(t);
-            const p1 = (p0 + 1) % POINTS.length;
-            const p2 = (p0 + 2) % POINTS.length;
-            const p3 = (p0 + 3) % POINTS.length;
-
-            t = t - Math.floor(t);
-            const t2 = t * t;
-            const t3 = t2 * t;
-
-            const q0 = -t3 + 2.0 * t2 - t;
-            const q1 = 3.0 * t3 - 5.0 * t2 + 2.0;
-            const q2 = -3.0 * t3 + 4.0 * t2 + t;
-            const q3 = t3 - t2;
-
-            const pq0x = POINTS[p0][0] * q0 * 0.5;
-            const pq1x = POINTS[p1][0] * q1 * 0.5;
-            const pq2x = POINTS[p2][0] * q2 * 0.5;
-            const pq3x = POINTS[p3][0] * q3 * 0.5;
-            const pq0y = POINTS[p0][1] * q0 * 0.5;
-            const pq1y = POINTS[p1][1] * q1 * 0.5;
-            const pq2y = POINTS[p2][1] * q2 * 0.5;
-            const pq3y = POINTS[p3][1] * q3 * 0.5;
-            const pq0z = POINTS[p0][2] * q0 * 0.5;
-            const pq1z = POINTS[p1][2] * q1 * 0.5;
-            const pq2z = POINTS[p2][2] * q2 * 0.5;
-            const pq3z = POINTS[p3][2] * q3 * 0.5;
-
-            return [
-                pq0x + pq1x + pq2x + pq3x,
-                pq0y + pq1y + pq2y + pq3y,
-                pq0z + pq1z + pq2z + pq3z,
-            ];
-        };
-
-        const SAMPLE_SIZE = 0.005;
-        const STEP = 0.0002;
-        const buildSegments = () => {
-            let segments = [];
-            let remainingLength = 0.0;
-            for (let i = 0.0; i < POINTS.length - SAMPLE_SIZE; i += SAMPLE_SIZE) {
-                const xyz1 = samplePath(i);
-                const xyz2 = samplePath(i + SAMPLE_SIZE);
-                const dx = xyz2[0] - xyz1[0];
-                const dy = xyz2[1] - xyz1[1];
-                const dz = xyz2[2] - xyz1[2];
-                const seglen = Math.sqrt(dx * dx + dy * dy + dz * dz);
-                remainingLength += seglen;
-                if (remainingLength >= STEP) {
-                    segments.push(i);
-                    remainingLength -= STEP;
-                }
-            }
-            return segments;
-        };
-        let segments = buildSegments();
-        const sampleSegment = alpha => {
-            const x = alpha * (segments.length - 1);
-            const bottom = Math.floor(x) % segments.length;
-            const top = (bottom + 1) % segments.length;
-
-            const bx = segments[bottom];
-            const tx = segments[top];
-
-            const frac = x - Math.floor(x);
-            const simfrac = 1.0 - frac;
-
-            const xyzb = samplePath(bx);
-            const xyzt = samplePath(tx);
-
-            const finalX = xyzb[0] * simfrac + xyzt[0] * frac;
-            const finalY = xyzb[1] * simfrac + xyzt[1] * frac;
-            const finalZ = xyzb[2] * simfrac + xyzt[2] * frac;
-
-            return {
-                x: finalX,
-                y: finalY,
-                z: finalZ,
-            };
-        };
-
-        const ANIMATION_DURATION_S = 120.0;
+        const ANIMATION_DURATION_S = 30.0;
         let oldTime = 0.0;
         let animationAlpha = 0.0;
         const loop = time => {
             const frameTime = (time - oldTime) / 1000.0;
             oldTime = time;
 
-            const cameraPoint = sampleSegment(animationAlpha);
+            // Interpolate the path to get the camera position
+            const result = interpolant.evaluate(animationAlpha);
 
+            // Update the camera position
             const camera = instance.view.camera;
-            camera.position.set(cameraPoint.x, cameraPoint.y, cameraPoint.z);
+            camera.position.set(result[0], result[1], result[2]);
             camera.lookAt(center);
             camera.updateMatrixWorld();
-
             instance.notifyChange(camera);
 
-            label.position.set(cameraPoint.x, cameraPoint.y, 0);
+            // Update the camera position in the minimap
+            label.position.set(result[0], result[1], 0);
             label.updateMatrixWorld();
             minimapInstance.notifyChange(label);
 
-            animationAlpha += frameTime / ANIMATION_DURATION_S;
-
+            animationAlpha = (animationAlpha + frameTime / ANIMATION_DURATION_S) % 1;
             requestAnimationFrame(loop);
         };
 
