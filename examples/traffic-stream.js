7235da2761874c0aabf9ca05d36f36acc25baa7f
Thomas Muguet <thomas.muguet@oslandia.com>
2026-06-26 11:43:13 +0200

    feat(examples): fix traffic-stream

--- examples/traffic-stream.js
+++ examples/traffic-stream.js
@@ -5,80 +5,108 @@
  */
 
 import { VideoTexture, PlaneGeometry, Mesh, MeshBasicMaterial, DoubleSide } from 'three';
+import { CSS2DObject } from 'three/examples/jsm/Addons.js';
 import { MapControls } from 'three/examples/jsm/controls/MapControls.js';
 
+import Coordinates from '@giro3d/giro3d/core/geographic/Coordinates.js';
 import CoordinateSystem from '@giro3d/giro3d/core/geographic/CoordinateSystem.js';
+import Extent from '@giro3d/giro3d/core/geographic/Extent.js';
 import Instance from '@giro3d/giro3d/core/Instance.js';
+import ColorLayer from '@giro3d/giro3d/core/layer/ColorLayer.js';
+import ElevationLayer from '@giro3d/giro3d/core/layer/ElevationLayer.js';
+import Map from '@giro3d/giro3d/entities/Map.js';
+import BilFormat from '@giro3d/giro3d/formats/BilFormat.js';
 import Inspector from '@giro3d/giro3d/gui/Inspector.js';
+import WmtsSource from '@giro3d/giro3d/sources/WmtsSource.js';
 
-const crs = CoordinateSystem.register(
-    'EPSG:32615',
-    `
-    PROJCRS["WGS 84 / UTM zone 15N",
-    BASEGEOGCRS["WGS 84",
-        ENSEMBLE["World Geodetic System 1984 ensemble",
-            MEMBER["World Geodetic System 1984 (Transit)"],
-            MEMBER["World Geodetic System 1984 (G730)"],
-            MEMBER["World Geodetic System 1984 (G873)"],
-            MEMBER["World Geodetic System 1984 (G1150)"],
-            MEMBER["World Geodetic System 1984 (G1674)"],
-            MEMBER["World Geodetic System 1984 (G1762)"],
-            MEMBER["World Geodetic System 1984 (G2139)"],
-            MEMBER["World Geodetic System 1984 (G2296)"],
-            ELLIPSOID["WGS 84",6378137,298.257223563,
-                LENGTHUNIT["metre",1]],
-            ENSEMBLEACCURACY[2.0]],
-        PRIMEM["Greenwich",0,
-            ANGLEUNIT["degree",0.0174532925199433]],
-        ID["EPSG",4326]],
-    CONVERSION["UTM zone 15N",
-        METHOD["Transverse Mercator",
-            ID["EPSG",9807]],
-        PARAMETER["Latitude of natural origin",0,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8801]],
-        PARAMETER["Longitude of natural origin",-93,
-            ANGLEUNIT["degree",0.0174532925199433],
-            ID["EPSG",8802]],
-        PARAMETER["Scale factor at natural origin",0.9996,
-            SCALEUNIT["unity",1],
-            ID["EPSG",8805]],
-        PARAMETER["False easting",500000,
-            LENGTHUNIT["metre",1],
-            ID["EPSG",8806]],
-        PARAMETER["False northing",0,
-            LENGTHUNIT["metre",1],
-            ID["EPSG",8807]]],
-    CS[Cartesian,2],
-        AXIS["(E)",east,
-            ORDER[1],
-            LENGTHUNIT["metre",1]],
-        AXIS["(N)",north,
-            ORDER[2],
-            LENGTHUNIT["metre",1]],
-    USAGE[
-        SCOPE["Navigation and medium accuracy spatial referencing."],
-        AREA["Between 96°W and 90°W, northern hemisphere between equator and 84°N, onshore and offshore. Canada - Manitoba; Nunavut; Ontario. Ecuador -Galapagos. Guatemala. Mexico. United States (USA)."],
-        BBOX[0,-96,84,-90]],
-    ID["EPSG",32615]]`,
+import StatusBar from './widgets/StatusBar';
+
+const epsg2154 = CoordinateSystem.register(
+    'EPSG:2154',
+    '+proj=lcc +lat_0=46.5 +lon_0=3 +lat_1=49 +lat_2=44 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs',
+);
+
+CoordinateSystem.register(
+    'IGNF:WGS84G',
+    'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]',
 );
 
 const instance = new Instance({
     target: 'view',
-    crs,
+    crs: epsg2154,
     backgroundColor: null,
 });
 
+const center = new Coordinates(CoordinateSystem.epsg4326, 5.680029, 45.215664).as(
+    instance.coordinateSystem,
+);
+const extent = Extent.fromCenterAndSize(epsg2154, { x: center.x, y: center.y }, 10_000, 10_000);
+
+const map = new Map({
+    extent,
+    backgroundColor: 'gray',
+    lighting: {
+        enabled: true,
+        hillshadeIntensity: 0.6,
+        elevationLayersOnly: true,
+    },
+});
+instance.add(map);
+
+const capabilitiesUrl =
+    'https://data.geopf.fr/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities';
+
+WmtsSource.fromCapabilities(capabilitiesUrl, {
+    layer: 'ELEVATION.ELEVATIONGRIDCOVERAGE.HIGHRES',
+    format: new BilFormat(),
+    noDataValue: -1000,
+})
+    .then(source => {
+        map.addLayer(
+            new ElevationLayer({
+                extent: map.extent,
+                resolutionFactor: 1 / 8,
+                minmax: { min: 500, max: 1500 },
+                source: source,
+            }),
+        );
+    })
+    .catch(console.error);
+
+WmtsSource.fromCapabilities(capabilitiesUrl, {
+    layer: 'HR.ORTHOIMAGERY.ORTHOPHOTOS',
+})
+    .then(source => {
+        map.addLayer(
+            new ColorLayer({
+                extent: map.extent,
+                source: source,
+            }),
+        );
+    })
+    .catch(console.error);
+
+instance.view.camera.position.set(914834, 6458031, 1812);
+instance.view.camera.rotation.set(1.096239934839674, 0.9166476093340195, 0.38709570855793624);
+instance.notifyChange(instance.view.camera);
+
+const controls = new MapControls(instance.view.camera, instance.domElement);
+controls.target.set(910331, 6460940, 0);
+controls.enableDamping = true;
+controls.dampingFactor = 0.2;
+controls.saveState();
+instance.view.setControls(controls);
+StatusBar.bind(instance);
+
 Inspector.attach('inspector', instance);
 
+// Embed the video in ThreeJS
 const video = document.getElementById('traffic-feed-mp4');
-
 if (!(video instanceof HTMLVideoElement)) {
     throw new Error('Expected a video');
 }
 
 const texture = new VideoTexture(video);
-// Create the material
 const movieMaterial = new MeshBasicMaterial({
     map: texture,
     color: 0xffffffff,
@@ -86,21 +114,44 @@ const movieMaterial = new MeshBasicMaterial({
     toneMapped: false,
 });
 
-const movieGeometry = new PlaneGeometry(10, 10);
+const movieGeometry = new PlaneGeometry(960, 540);
 const movieScreen = new Mesh(movieGeometry, movieMaterial);
-movieScreen.position.set(576715, 5188162, 200);
+movieScreen.position.set(910331, 6460940, 550);
+movieScreen.rotation.set(Math.PI / 2, 0.98, 0);
 movieScreen.updateMatrixWorld();
 
 instance.add(movieScreen).catch(console.error);
 
-const controls = new MapControls(instance.view.camera, instance.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.2;
-controls.saveState();
-instance.view.setControls(controls);
+// Add a label for displaying the video's position
+const labelElement = document.createElement('div');
+labelElement.style.borderRadius = '50%';
+labelElement.style.borderWidth = '2px';
+labelElement.style.borderStyle = 'solid';
+labelElement.style.width = '28px';
+labelElement.style.height = '28px';
+labelElement.style.textAlign = 'center';
+labelElement.style.verticalAlign = 'middle';
+labelElement.style.backgroundColor = 'orange';
+labelElement.style.pointerEvents = 'auto';
+labelElement.style.cursor = 'pointer';
+
+const cameraSymbol = document.createElement('i');
+cameraSymbol.classList.add('bi', 'bi-camera-video-fill', 'text-dark', 'shadow');
+
+labelElement.appendChild(cameraSymbol);
+
+labelElement.addEventListener('click', () => {
+    movieScreen.visible = !movieScreen.visible;
+});
+
+const label = new CSS2DObject(labelElement);
+label.visible = true;
+label.position.set(910331, 6460940, 208);
+label.updateMatrixWorld();
+instance.add(label);
 
 const animate = () => {
-    texture.needsUpdate = true;
+    // We need to notify changes for Giro3D to keep rendering
     instance.notifyChange();
     requestAnimationFrame(animate);
 };
